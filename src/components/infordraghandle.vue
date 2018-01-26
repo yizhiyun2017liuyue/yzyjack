dc68222e812935b93a4637044c2d8b20e12959e3
GracefulTing <953632795@qq.com>
2018-01-26 09:23:40 +0800

    杨婷提交

--- src/components/infordraghandle.vue
+++ src/components/infordraghandle.vue
@@ -2,12 +2,33 @@
     <div>
     	<p>测试</p>
    	    <router-link to="/aboutShow">第二个页面</router-link>
+   	    <canvas id="canvas" width="300" height="300"></canvas>
 	</div>
 </template>
 
 <script>
 export default {
-  name: 'infordraghandle'
+  	name: 'infordraghandle',
+  	mounted(){
+  		var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+        ctx.lineWidth = 6;
+        canvas.addEventListener('touchstart',function(event){   //触摸点按下事件
+            if (event.targetTouches.length == 1) {
+                var touch = event.targetTouches[0];
+                ctx.beginPath();
+                ctx.moveTo(touch.clientX-canvas.offsetLeft,touch.clientY-canvas.offsetTop);
+                canvas.addEventListener('touchmove',function (event) {  //手机拖动触摸点事件
+                    var touche = event.targetTouches[0];
+                    ctx.lineTo(touche.clientX - canvas.offsetLeft, touche.clientY - canvas.offsetTop);
+                    ctx.stroke();
+                },false)
+                canvas.addEventListener('touchend',function (event) {  //手机离开屏幕的事件
+                    ctx.closePath();
+                },false)
+            }
+        },false)
+  	}
 }
 </script>
 
--- src/components/information.vue
+++ src/components/information.vue
@@ -2,12 +2,18 @@
     <div>
 	    <img src="/static/images/logo.png">
    	    <router-link to="/infordraghandle">下一个页面</router-link>
+   	    <input type="button" value="马上开启" id="btn" v-on:click="changePage">
 	</div>
 </template>
 
 <script>
 export default {
-  name: 'information'
+ 	name: 'information',
+  	methods:{
+  		changePage(){
+  			this.$router.push({name: 'infordraghandle'});
+  		}
+  	}
 }
 </script>
 
